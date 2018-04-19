03a80702e10f81cfaf0eb423d304c05fc9a0ed51
lamarque.lucien <lamarque.lucien@P6322-016.SIO2>
2018-04-19 10:29:12 +0200

    weel

--- index.php
+++ index.php
@@ -7,11 +7,50 @@ if ($matricule == 0) {
     exit();
 }
 
+
+if ($role == "ASSISTANT"){ ?>
+    <div class="page-header">
+    <h1>Bienvenue</h1>
+    </div>
+    <div class="well">
+    <p>Vous êtes connecté sur l'interface de gestion de CashCash en tant qu'assistant.</p>
+    <p>Vous pouvez donc : <p>
+    <ul>
+    <li>Affecter les visites aux techniciens.</li> 
+    <li>Visualiser ou modifier les fiches clients.</li> 
+    <li>Génerer des fiches d'interventions au format PDF.</li> 
+    <li>Consulter les interventions et obtenir les statistiques des techniciens.</li> 
+    </ul>
+      </div>  
+    <?php 
+}else {
+    ?>
+    
+        <div class="page-header">
+    <h1>Bienvenue</h1>
+    </div>
+    <div class="well">
+        <p>Vous êtes connecté sur l'interface de gestion de CashCash en tant que technicien.</p>
+    <p>Vous pouvez donc : <p>
+    <ul>
+    <li>Consulter vos interventions</li> 
+    <li>Valider une intervention.</li> 
+    
+    </ul>
+    
+      </div> 
+     <?php 
+}
+    
+    
+
+
+
 ?>
-            
+   
 
 </div>
-</div>
+
 </body>
 
 </html>
